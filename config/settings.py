9cdbe38e9cfbc54b38cc30f969417d6572dd69eb
ChrisAchinga <achinga.chris@gmail.com>
2021-02-01 07:07:09 +0300

    allowed hosting

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = "CHANGE_ME!!!! (P.S. the SECRET_KEY environment variable will be us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://coastwoman.herokuapp.com']
 
 
 # Application definition
