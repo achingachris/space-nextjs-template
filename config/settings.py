643074bcdb7744105770a0c84232c1da3d8127b4
ChrisAchinga <achinga.chris@gmail.com>
2021-02-10 09:04:54 +0300

    settings

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,8 @@ import os
 import django_heroku
 # import environ
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # env = environ.Env()
 # reading .env file
 # environ.Env.read_env()
