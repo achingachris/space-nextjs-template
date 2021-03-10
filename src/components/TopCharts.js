f22755d4a984f492614c01692e886522332a1b97
ChrisAchinga <achinga.chris@gmail.com>
2021-03-10 23:58:36 +0300

    clean charts

--- src/components/TopCharts.js
+++ src/components/TopCharts.js
@@ -139,25 +139,25 @@ const TopCharts = () => {
     })
   })
   return (
-    <div class='row'>
-      <div class='col-xl-6'>
-        <div class='card mb-4'>
-          <div class='card-header'>
-            <i class='fas fa-chart-area mr-1'></i>
+    <div className='row'>
+      <div className='col-xl-6'>
+        <div className='card mb-4'>
+          <div className='card-header'>
+            <i className='fas fa-chart-area mr-1'></i>
             Area Chart Example
           </div>
-          <div class='card-body'>
+          <div className='card-body'>
             <canvas id='myAreaChart' width='100%' height='40'></canvas>
           </div>
         </div>
       </div>
-      <div class='col-xl-6'>
-        <div class='card mb-4'>
-          <div class='card-header'>
-            <i class='fas fa-chart-bar mr-1'></i>
+      <div className='col-xl-6'>
+        <div className='card mb-4'>
+          <div className='card-header'>
+            <i className='fas fa-chart-bar mr-1'></i>
             Bar Chart Example
           </div>
-          <div class='card-body'>
+          <div className='card-body'>
             <canvas id='myBarChart' width='100%' height='40'></canvas>
           </div>
         </div>
