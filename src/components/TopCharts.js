690e95f49aed478e28645cd27cec7dcfa01b75dc
ChrisAchinga <achinga.chris@gmail.com>
2021-03-10 23:51:18 +0300

    update charts to dummy data

--- src/components/TopCharts.js
+++ src/components/TopCharts.js
@@ -2,53 +2,139 @@ import { useEffect } from 'react'
 import Chart from 'chart.js'
 
 const TopCharts = () => {
+  // line graph chart
   useEffect(() => {
     const ctx = document.getElementById('myAreaChart')
     new Chart(ctx, {
-      type: 'pie',
+      type: 'line',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: [
+          'Mar 1',
+          'Mar 2',
+          'Mar 3',
+          'Mar 4',
+          'Mar 5',
+          'Mar 6',
+          'Mar 7',
+          'Mar 8',
+          'Mar 9',
+          'Mar 10',
+          'Mar 11',
+          'Mar 12',
+          'Mar 13',
+        ],
         datasets: [
           {
-            label: 'Revenue Collected',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'Red',
-              'Blue',
-              'Yellow',
-              'Green',
-              'Purple',
-              'Orange',
+            label: 'Sessions',
+            lineTension: 0.3,
+            backgroundColor: 'rgba(2,117,216,0.2)',
+            borderColor: 'rgba(2,117,216,1)',
+            pointRadius: 5,
+            pointBackgroundColor: 'rgba(2,117,216,1)',
+            pointBorderColor: 'rgba(255,255,255,0.8)',
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(2,117,216,1)',
+            pointHitRadius: 50,
+            pointBorderWidth: 2,
+            data: [
+              10000,
+              30162,
+              26263,
+              18394,
+              18287,
+              28682,
+              31274,
+              33259,
+              25849,
+              24159,
+              32651,
+              31984,
+              38451,
             ],
-            borderColor: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            borderWidth: 1,
           },
         ],
       },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              time: {
+                unit: 'date',
+              },
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                maxTicksLimit: 7,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 40000,
+                maxTicksLimit: 5,
+              },
+              gridLines: {
+                color: 'rgba(0, 0, 0, .125)',
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
+      },
     })
   })
+  // bar graph
   useEffect(() => {
     const ctx = document.getElementById('myBarChart')
     new Chart(ctx, {
       type: 'bar',
       data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
         datasets: [
           {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'Red',
-              'Blue',
-              'Yellow',
-              'Green',
-              'Purple',
-              'Orange',
-            ],
-            borderColor: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            borderWidth: 1,
+            label: 'Revenue',
+            backgroundColor: 'rgba(2,117,216,1)',
+            borderColor: 'rgba(2,117,216,1)',
+            data: [4215, 5312, 6251, 7841, 9821, 14984],
           },
         ],
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              time: {
+                unit: 'month',
+              },
+              gridLines: {
+                display: false,
+              },
+              ticks: {
+                maxTicksLimit: 6,
+              },
+            },
+          ],
+          yAxes: [
+            {
+              ticks: {
+                min: 0,
+                max: 15000,
+                maxTicksLimit: 5,
+              },
+              gridLines: {
+                display: true,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
       },
     })
   })
