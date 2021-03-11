197a7fcb1af0d03e31071c836308eac4bde68c44
ChrisAchinga <achinga.chris@gmail.com>
2021-03-11 12:56:36 +0300

    property data

---
+++ src/properties.js
@@ -0,0 +1,18 @@
+const properties = [
+  {
+    id: 101,
+    name: 'Waiyaki 1',
+    no_of_units: 50,
+    owner: 'Chong Chip',
+    no_of_tenats: 33,
+  },
+  {
+    id: 931,
+    name: 'Waiyaki 2',
+    no_of_units: 50,
+    owner: 'Ping Pong',
+    no_of_tenats: 43,
+  },
+]
+
+export default properties
