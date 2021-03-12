e3e0d7f98eac029d7757c61c877bbd569641a5e9
ChrisAchinga <achinga.chris@gmail.com>
2021-03-12 11:06:02 +0300

    add new modal

--- src/pages/Properties.js
+++ src/pages/Properties.js
@@ -1,12 +1,49 @@
+import { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import Header from '../components/Header'
 import properties from '../properties'
 
 const Properties = () => {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
   return (
     <main>
       <div className='container-fluid'>
         <Header page='Properties' path='home/properties' />
+        <Button
+          variant='success'
+          size='lg'
+          block
+          className='mb-5'
+          onClick={handleShow}
+        >
+          Add New Property
+        </Button>
+        {/* modal form */}
+        <Modal
+          show={show}
+          onHide={handleClose}
+          size='lg'
+          aria-labelledby='contained-modal-title-vcenter'
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant='warning' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='success' onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        
         <div class='card mb-4'>
           <div class='card-header'>
             <i class='fas fa-table mr-1'></i>
--- src/pages/Tenants.js
+++ src/pages/Tenants.js
@@ -1,12 +1,49 @@
+import { useState } from 'react'
+import { Button, Modal } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import Header from '../components/Header'
 import tenants from '../tenants'
 
 const Tenants = () => {
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   return (
     <main>
       <div className='container-fluid'>
         <Header page='Tenants' path='home/tenants' />
+        <Button
+          variant='success'
+          size='lg'
+          block
+          className='mb-5'
+          onClick={handleShow}
+        >
+          Add New Tenant
+        </Button>
+        {/* modal form */}
+        <Modal
+          show={show}
+          onHide={handleClose}
+          size='lg'
+          aria-labelledby='contained-modal-title-vcenter'
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant='warning' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='success' onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <div class='card mb-4'>
           <div class='card-header'>
             <i class='fas fa-table mr-1'></i>
