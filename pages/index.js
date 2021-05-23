a5e650191020825dbb9e8d89d63a4d2d664b928b
ChrisAchinga <achinga.chris@gmail.com>
2021-05-23 12:01:13 +0300

    home pgae

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
 import Layout from '@/layouts/Layout'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <Layout>
-      Waiyaki Homes
+      <Container>
+        <h1 className='text-center text-muted'>Our Assets</h1>
+        <Row className='justify-content-center text-center align-content-center'>
+          <Col md={4} className='m-3'>
+            <Card>Buidings</Card>
+          </Col>
+          <Col md={4} className='m-3'>
+            <Card>residential units</Card>
+          </Col>
+          <Col md={4} className='m-3'>
+            <Card>self service online system</Card>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   )
 }
