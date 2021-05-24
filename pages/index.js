3f5fa2b867a6e88b260dcee97be88a63377bc322
ChrisAchinga <achinga.chris@gmail.com>
2021-05-24 11:36:11 +0300

    home page dummy setup

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,53 @@
 import Layout from '@/layouts/Layout'
+import Image from 'next/image'
 import { Container, Row, Col, Card } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <Layout>
-      <Container>
-        <h1 className='text-center text-muted'>Our Assets</h1>
-        <Row className='justify-content-center text-center align-content-center'>
-          <Col md={4} className='m-3'>
-            <Card>Buidings</Card>
-          </Col>
-          <Col md={4} className='m-3'>
-            <Card>residential units</Card>
-          </Col>
-          <Col md={4} className='m-3'>
-            <Card>self service online system</Card>
-          </Col>
-        </Row>
-      </Container>
+      <h1 className='text-center text-muted'>Our Assets</h1>
+      <Row className='justify-content-center text-center align-content-center'>
+        <Col lg={6} md={6} className='m-3'>
+          <Card>
+            <a href='#'>
+              <Image
+                class='card-img-top'
+                src='/rental.jpg'
+                width={700}
+                height={400}
+                alt=''
+              />
+            </a>
+            <Card.Body>
+              <Card.Title>Residential Appartments</Card.Title>
+            </Card.Body>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              viverra euismod odio, gravida pellentesque urna varius vitae.
+            </Card.Text>
+          </Card>
+        </Col>
+        <Col lg={6} md={6} className='m-3'>
+        <Card>
+            <a href='#'>
+              <Image
+                class='card-img-top'
+                src='/furnish.jpg'
+                width={700}
+                height={400}
+                alt=''
+              />
+            </a>
+            <Card.Body>
+              <Card.Title>Furnished Rooms</Card.Title>
+            </Card.Body>
+            <Card.Text>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam
+              viverra euismod odio, gravida pellentesque urna varius vitae.
+            </Card.Text>
+          </Card>
+        </Col>
+      </Row>
     </Layout>
   )
 }
