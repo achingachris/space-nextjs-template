7d495671f9ff7ed6a6c4a88c16df95e6738b4032
achingachris <achinga.chris@gmail.com>
2023-01-11 15:15:32 +0300

    configure scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
+import '../styles/scss/bootstrap.scss'
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
   return <Component {...pageProps} />
 }
